containers/libkrun

Add X11 cross domain channel type.
This adds the X11 cross domain channel and an ability to share
futexes between the vm and the host.

Signed-off-by: Sasha Finkelstein <fnkl.kernel@gmail.com>

src/rutabaga_gfx/src/cross_domain/cross_domain_protocol.rs
@@ -18,10 +18,14 @@ pub const CROSS_DOMAIN_CMD_SEND: u8 = 4;
 pub const CROSS_DOMAIN_CMD_RECEIVE: u8 = 5;
 pub const CROSS_DOMAIN_CMD_READ: u8 = 6;
 pub const CROSS_DOMAIN_CMD_WRITE: u8 = 7;
+pub const CROSS_DOMAIN_CMD_FUTEX_NEW: u8 = 8;
+pub const CROSS_DOMAIN_CMD_FUTEX_SIGNAL: u8 = 9;
+pub const CROSS_DOMAIN_CMD_FUTEX_DESTROY: u8 = 10;
 
 /// Channel types (must match rutabaga channel types)
 pub const CROSS_DOMAIN_CHANNEL_TYPE_WAYLAND: u32 = 0x0001;
 pub const CROSS_DOMAIN_CHANNEL_TYPE_CAMERA: u32 = 0x0002;
+pub const CROSS_DOMAIN_CHANNEL_TYPE_X11: u32 = 0x0011;
 
 /// The maximum number of identifiers (value inspired by wp_linux_dmabuf)
 pub const CROSS_DOMAIN_MAX_IDENTIFIERS: usize = 4;
@@ -36,6 +40,8 @@ pub const CROSS_DOMAIN_ID_TYPE_READ_PIPE: u32 = 3;
 /// ID for Wayland pipe used for writing.  The writing is done by the guest and the host proxy.
 /// The host receives the write end of the pipe over the host Wayland socket.
 pub const CROSS_DOMAIN_ID_TYPE_WRITE_PIPE: u32 = 4;
+
+pub const CROSS_DOMAIN_ID_TYPE_SHM: u32 = 5;
 
 /// No ring
 pub const CROSS_DOMAIN_RING_NONE: u32 = 0xffffffff;
@@ -118,4 +124,29 @@ pub struct CrossDomainReadWrite {
     pub opaque_data_size: u32,
     pub pad: u32,
     // Data of size "opaque data size follows"
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Default, AsBytes, FromBytes)]
+pub struct CrossDomainFutexNew {
+    pub hdr: CrossDomainHeader,
+    pub filename: u64,
+    pub id: u32,
+    pub pad: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Default, AsBytes, FromBytes)]
+pub struct CrossDomainFutexSignal {
+    pub hdr: CrossDomainHeader,
+    pub id: u32,
+    pub pad: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Default, AsBytes, FromBytes)]
+pub struct CrossDomainFutexDestroy {
+    pub hdr: CrossDomainHeader,
+    pub id: u32,
+    pub pad: u32,
 }

ThousandBirdsInc/thousandbirds-libkrun

Co-developed-by: Asahi Lina <lina@asahilina.net>
Signed-off-by: Asahi Lina <lina@asahilina.net>

src/rutabaga_gfx/src/cross_domain/cross_domain_protocol.rs
@@ -18,10 +18,14 @@ pub const CROSS_DOMAIN_CMD_SEND: u8 = 4;
 pub const CROSS_DOMAIN_CMD_RECEIVE: u8 = 5;
 pub const CROSS_DOMAIN_CMD_READ: u8 = 6;
 pub const CROSS_DOMAIN_CMD_WRITE: u8 = 7;
+pub const CROSS_DOMAIN_CMD_FUTEX_NEW: u8 = 8;
+pub const CROSS_DOMAIN_CMD_FUTEX_SIGNAL: u8 = 9;
+pub const CROSS_DOMAIN_CMD_FUTEX_DESTROY: u8 = 10;
 
 /// Channel types (must match rutabaga channel types)
 pub const CROSS_DOMAIN_CHANNEL_TYPE_WAYLAND: u32 = 0x0001;
 pub const CROSS_DOMAIN_CHANNEL_TYPE_CAMERA: u32 = 0x0002;
+pub const CROSS_DOMAIN_CHANNEL_TYPE_X11: u32 = 0x0011;
 
 /// The maximum number of identifiers (value inspired by wp_linux_dmabuf)
 pub const CROSS_DOMAIN_MAX_IDENTIFIERS: usize = 4;
@@ -36,6 +40,8 @@ pub const CROSS_DOMAIN_ID_TYPE_READ_PIPE: u32 = 3;
 /// ID for Wayland pipe used for writing.  The writing is done by the guest and the host proxy.
 /// The host receives the write end of the pipe over the host Wayland socket.
 pub const CROSS_DOMAIN_ID_TYPE_WRITE_PIPE: u32 = 4;
+
+pub const CROSS_DOMAIN_ID_TYPE_SHM: u32 = 5;
 
 /// No ring
 pub const CROSS_DOMAIN_RING_NONE: u32 = 0xffffffff;
@@ -118,4 +124,30 @@ pub struct CrossDomainReadWrite {
     pub opaque_data_size: u32,
     pub pad: u32,
     // Data of size "opaque data size follows"
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Default, AsBytes, FromBytes)]
+pub struct CrossDomainFutexNew {
+    pub hdr: CrossDomainHeader,
+    pub fs_id: u64,
+    pub handle: u64,
+    pub id: u32,
+    pub pad: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Default, AsBytes, FromBytes)]
+pub struct CrossDomainFutexSignal {
+    pub hdr: CrossDomainHeader,
+    pub id: u32,
+    pub pad: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Default, AsBytes, FromBytes)]
+pub struct CrossDomainFutexDestroy {
+    pub hdr: CrossDomainHeader,
+    pub id: u32,
+    pub pad: u32,
 }